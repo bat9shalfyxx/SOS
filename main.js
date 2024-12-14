100fee0eb84604cd7ce3ed8038dfc39cd75d04ef
bat9shalfyxx <shokirovttt192@gmail.com>
2024-12-14 20:19:38 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const navListEl = document.querySelectorAll("nav a, .hader-scroll-down a");
+
+for (const element of navListEl) {
+    element.addEventListener("click", (event) => {
+        event.preventDefault();
+        const targetID = this.getAttribute("href");
+        const targetEl = document.querySelector(targetID);
+        if (targetEl) {
+            targetEl.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+    });
+}
